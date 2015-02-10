d0f1f2ec33d19617d1ef0dc7bbe0c21b627bfce7
Pat Schloss <pschloss@umich.edu>
2015-02-10 09:52:36 -0500

    Fix layout issues

--- code/get_phylum_plot.R
+++ code/get_phylum_plot.R
@@ -1,4 +1,4 @@
-cairo_pdf("test.pdf", width=4, height=7)
+#cairo_pdf("test.pdf", width=4, height=7)
 
 source("code/rf_baseline_analysis.R")
 
@@ -29,7 +29,7 @@ otu_plot <- function(rabunds, ordered_treatments, otu_label){
     means <- aggregate(rabunds, by=list(baseline_treatment), mean)$x
     o <- c(3, 6, 4, 8, 2, 7, 1, 5)
 
-    par(mar=c(0.5, 2, 0.5, 0.5))
+    par(mar=c(0.75, 2, 0.75, 0.5))
 
     plot(NA, xlim=c(0.75,8.25), xlab="", ylim=c(0,max(rabunds)), axes=F, cex.lab=1.4)
 
@@ -44,7 +44,7 @@ otu_plot <- function(rabunds, ordered_treatments, otu_label){
         pch=pch[ordered_treatments[i]], jitter=0.25, cex=1.5, add=T)
     }
 
-    text(x=0.35, y=0.95*max(rabunds), label=otu_label, pos=4, font=2, cex=1)
+    text(x=0.35, y=1.12*max(rabunds), label=otu_label, pos=4, font=2, cex=1, xpd=TRUE)
 
 }
 
@@ -52,7 +52,7 @@ design <- matrix(c(1,2,3,4), ncol=1)
 design <- cbind(c(5,5,5,5), design)
 design <- rbind(design, c(0,6))
 
-layout(design, width=c(0.13, 1), height=c(1,1,1,1,0.8))
+layout(design, width=c(0.13, 1), height=c(1,1,1,1,1.2))
 
 otu_plot(baseline_rabund[,ordered_otus[1]], ordered_treatments, tax[ordered_otus[1],"Taxonomy"])
 otu_plot(baseline_rabund[,ordered_otus[2]], ordered_treatments, tax[ordered_otus[2],"Taxonomy"])
@@ -61,8 +61,9 @@ otu_plot(baseline_rabund[,ordered_otus[4]], ordered_treatments, tax[ordered_otus
 
 plot.new()
 text(x=0.7, y=0.5, label="Relative abundance (%)", cex=1.5, srt=90)
+text(label="B", x=0.0, y=1.03, font=2.5, cex=2.5, xpd=TRUE)
 
 plot.new()
-text(x=seq(0.05,1,length=8), y=1, labels[ordered_treatments], srt=90, cex=1.2, pos=2)
+text(x=seq(0.05,1,length=8), y=1, labels[ordered_treatments], srt=60, cex=1.0, pos=2, xpd=TRUE)
 
-dev.off()
+#dev.off()
