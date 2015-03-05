a7fc2bbd8b5be48540f48e7255a92e98bc93336d
Pat Schloss <pschloss@umich.edu>
2015-03-05 10:57:36 -0500

    Change heading labels to % Increase MSE

--- code/rf_baseline_analysis.R
+++ code/rf_baseline_analysis.R
@@ -189,7 +189,7 @@ plot_importance <- function(forest){
     abline(h=1:length(sorted_importance), lty=3, col="gray")
     points(x=rev(sorted_importance), y=1:length(sorted_importance), pch=19)
     mtext(side=2, line=8.25, adj=0, at=1:length(sorted_importance), text=rev(otu_tax_labels[names(sorted_importance)]), las=2, cex=0.7)
-    mtext(side=1, text="Decrease in MSE", line=2.0)
+    mtext(side=1, text="% Increase in MSE", line=2.0)
 
 }
 
@@ -233,10 +233,10 @@ plot_baseline_features <- function(tumor_counts, forest, rabund, treatment){
     tax$Taxonomy <- gsub(".*;", "", tax$Taxonomy)
 
     #let's just use the top six OTUs, based on our inspection of the importance plot
-    #and combine the OTU name with its taxonomy and Mean Decrease in MSE coefficient
+    #and combine the OTU name with its taxonomy and % Increase in MSE coefficient
     otus <- rownames(sorted_importance)[1:6]
     pretty_otus <- gsub("Otu0*", "OTU ", otus)
-    otu_labels <- paste0("(", pretty_otus, ")", "\nDecrease in MSE: ", format(round(sorted_importance[1:6,"%IncMSE"], 1), 1))
+    otu_labels <- paste0("(", pretty_otus, ")", "\n% Increase in MSE: ", format(round(sorted_importance[1:6,"%IncMSE"], 1), 1))
     otu_labels <- paste(tax[otus,2], otu_labels, sep=" ")
 
 
@@ -319,10 +319,10 @@ plot_final_features <- function(tumor_counts, forest, rabund, treatment){
     tax$Taxonomy <- gsub(".*;", "", tax$Taxonomy)
 
     #let's just use the top six OTUs, based on our inspection of the importance plot
-    #and combine the OTU name with its taxonomy and Mean Decrease in MSE
+    #and combine the OTU name with its taxonomy and % Increase in MSE
     otus <- rownames(sorted_importance)[1:7]
     pretty_otus <- gsub("Otu0*", "OTU ", otus)
-    otu_labels <- paste0("(", pretty_otus, ")", "\nDecrease in MSE: ", format(round(sorted_importance[1:7,"%IncMSE"], 1), 1))
+    otu_labels <- paste0("(", pretty_otus, ")", "\n% Increase in MSE: ", format(round(sorted_importance[1:7,"%IncMSE"], 1), 1))
     otu_labels <- paste(tax[otus,2], otu_labels, sep=" ")
 
 
