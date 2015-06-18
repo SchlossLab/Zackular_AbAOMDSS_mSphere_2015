9d525bfe771a6af53f3a14850721ed9fb78573ef
Pat Schloss <pschloss@umich.edu>
2015-06-18 07:43:55 -0400

    Fix labeling on Figure 5

--- code/build_figure_5.R
+++ code/build_figure_5.R
@@ -5,7 +5,7 @@ pdf("results/figures/figure_5.pdf", width=3.5, height=3.5)
 abx <- read.table(file="data/process/tumor_counts.tsv", header=T)
 intervention <- read.table(file="data/process/intervention_tumor_counts.tsv", header=T)
 
-layout(matrix(c(1,2), byrow=T, nrow=1), width=c(1.5,1.20), height=1)
+layout(matrix(c(1,2), byrow=T, nrow=1), width=c(1.5,1.5), height=1)
 
 par(mar=c(6,4,0.5,0.25))
 
@@ -19,7 +19,7 @@ stripchart(at=3, x=intervention[intervention$experiment=="Intervention1", "n_tum
 stripchart(at=4, x=intervention[intervention$experiment=="Intervention2", "n_tumors"],
     vertical=T, method="jitter", col="black", pch=17, jitter=0.15, cex=1.5, add=T)
 
-text(y=-9, x=(1:4)-0.6, label=c("No antibiotics", "All antibiotics", "Intervention 1", "Intervention 2"), srt=70, cex=0.8, xpd=TRUE)
+text(y=-9, x=(1:4)-0.6, label=c("No antibiotics", "All antibiotics", "Intervention 2", "Intervention 3"), srt=70, cex=0.8, xpd=TRUE)
 axis(2, las=1)
 axis(1, tick=TRUE, labels=FALSE)
 box()
